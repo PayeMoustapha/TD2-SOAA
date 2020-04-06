0ffee3a440f31f021b6eb3fe09a444e5ea4103d2
Moustapha <44175050+taphisko@users.noreply.github.com>
2020-04-06 18:00:26 +0000

    My Firts Commit

---
+++ ProjetOperationnel/src/main/java/HelloWorldApp/HelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWorldApp;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println ("Bonjour tout le monde!"); // Afficher la chaîne bonjour.
+		System.out.println ("Bye Bye !");             //Afficher la chaine Bye Bye.
+	}
+
+}
